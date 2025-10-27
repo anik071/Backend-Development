eddc28b98cfa1e27a39126096da44d3f78646687
Abdul Kader Anik <153476736+anik071@users.noreply.github.com>
2025-10-27 17:05:42 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// importing directory file
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "../backend mid/routes/user.route.js";
+import productRoute from "../backend mid/routes/product.route.js";
+import orderRoute from "../backend mid/routes/order.route.js";
+import cartRoute from "../backend mid/routes/cart.route.js";
+//middle ware
+const app=express();
+dotenv.config();
+app.use(express.json());
+app.use("/api",userRoute);
+app.use("/api",productRoute);
+app.use("/api",orderRoute);
+app.use("/api",cartRoute);
+
+
+const port=process.env.PORT || 8000;
+app.get("/",(req,res)=>{
+    res.send("Welcome to Our Backend");
+})
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB is connected Successfully");
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+app.listen(port,()=>{
+    console.log(`server is running in http://localhost:${port}`);
+})
