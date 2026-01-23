b4f2d9805382584550d48cb60aa4231f4f519353
Abdul Kader Anik <153476736+anik071@users.noreply.github.com>
2026-01-23 11:22:31 +0600

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,85 +1,214 @@
 import User from "../models/user.model.js";
-import { z } from "zod";
+import Cart from "../models/cart.model.js"
+import { success, z } from "zod";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import dotenv, { configDotenv } from "dotenv";
+configDotenv();
+const generateToken=(userId)=>{
+    return  jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES});
+}
 
-const userValidator =z.object({
-    name:z.string().min(2,"Name is too short."),
-    email:z.string().email("Invalid email address."),
-    age: z.number().int().positive("Age must be a postive integer. "),
-    password:z.string().min(7,"password must contain 8-20 charachter"),
-    address: z.object({
-    street: z.string().optional(),
-    city: z.string().optional(),
-    country: z.string().optional(),
-  }).optional(),
-  role: z.enum(["user", "admin"]).default("user"),
-  isActive: z.boolean().default(true),
-  hobbies: z.array(z.string()).optional(),
-  posts: z.array(z.string()).optional(),
-});
-
-export const getUsers = async (req, res) => {
+export const register=async(req,res)=>{
   try {
-    const users = await User.find(); 
-    res.status(200).json(users);
+      const {username,email}=req.body;
+      const exist=await User.findOne({
+        $or :[{email:email.toLowerCase()},{username:username.toLowerCase()}]
+      });
+      if(exist) {
+        return res.status(409).json({
+          success:false,
+          message:exist.username===username?"Username Already Taken":"Email Already Registered"
+        });
+      }
+      const user_body={
+        name:req.body.name,
+        username,
+        email,
+        age:req.body.age,
+        password:req.body.password,
+        role:req.body.role||user,
+        isActive:req.body.isActive||true,
+      }
+      if (req.body.address) {
+  user_body.address = req.body.address;
+}
+  const user=await User.create(user_body);
+  const cart=await Cart.create({
+    userID:user._id,
+    items:[]
+  });
+  const token = generateToken(user._id);
+  res.cookie('token',token,{
+    httpOnly:true,
+    secure:process.env.NODE_ENV==='production',
+    sameSite:'Strict',
+    maxAge: 7*24*60*60*1000
+  });
+  res.status(201).json({
+    success:true,
+            message:"User Registered Successfully",
+            data:{
+                user:{
+                    id: user._id,
+                    name: user.name,
+                    username: user.username,
+                    email: user.email,
+                    role: user.role
+                },
+                token
+            }
+  })
   } catch (error) {
-    res.status(500).json({ message: "Error fetching users", error: error.message });
+     res.status(500).json({
+            success:false,
+            message:"Error registering user",
+            error:error.message
+        });
   }
-};
+}
 
-export const getUser = async (req, res) => {
+export const login=async (req,res)=>{
   try {
-    const userId = req.params.id;
-    const user = await User.findById(userId); 
-
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Error fetching user", error: error.message });
-  }
-};
-
-export const createUser = async (req, res) => {
-  try {
-    const parsedData = userValidator.parse(req.body);
-    const newUser = await User.create(parsedData);
-    res.status(201).json(newUser);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return res.status(400).json({ errors: error.errors });
+    const {identifier,password}=req.body;
+    const user= await User.findOne({
+      $or :[{email:identifier.toLowerCase()},{username:identifier.toLowerCase()}]
+    });
+    if(!user){
+      return res.status(404).json({
+        success:false,
+        message:"User not found with this email or username"
+      });
     }
-    res.status(500).json({ message: "Error creating user", error: error.message });
-  }
-};
-
-export const updateUser = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const parsedData = userValidator.parse(req.body); // Validate input
-
-    
-    const updatedUser = await User.findByIdAndUpdate(userId, parsedData, { new: true });
-
-    if (!updatedUser) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return res.status(400).json({ errors: error.errors });
+    const match=await user.comparePassword(password);
+    if(!match){
+      return res.status(401).json({
+        success:false,
+        message:"Invalid Password"
+      });
     }
-    res.status(500).json({ message: "Error updating user", error: error.message });
-  }
-};
-
-export const deleteUser = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const deletedUser = await User.findByIdAndDelete(userId);
-
-    if (!deletedUser) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json({ message: "User deleted successfully" });
+    const token=generateToken(user._id);
+    res.cookie('token',token,{
+      httpOnly:true,
+      secure:process.env.NODE_ENV==='Production',
+      sameSite:'strict',
+      maxAge:7*24*60*60*1000
+    });
+    res.status(200).json({
+      success:true,
+            message:'Login Successful',
+            data:{
+                user:{
+                    id:user._id,
+                    username:user.username,
+                    email:user.email,
+                    role:user.role
+                },
+                token
+            }
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error deleting user", error: error.message });
+     res.status(500).json({
+            success:false,
+            message:"Error Logging in",
+            error:error.message
+        });
   }
+  
 };
+
+export const logout=async(req,res)=>{
+    try {
+        res.clearCookie('token'),
+        res.status(200).json({
+            success:true,
+            message:'Logged Out Successfully'
+        });
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            error:error.message
+        });
+    }
+};
+export const getProfile=async(req,res)=>{
+    try {
+        res.status(200).json({
+            success:true,
+            data:{
+                user:req.user
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            error:error.message
+        });
+    }
+};
+export const updateProfile=async(req,res)=>{
+     try {
+        const updatedData= {
+                name:req.body.name,
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password,
+                role:req.body.role||'user',
+                isActive:req.body.isActive||true
+            }
+if (req.body.address) {
+    updatedData.address = req.body.address;
+}
+         const user=await User.findByIdAndUpdate(
+            req.user._id,
+            updatedData
+           ,
+            {new:true,runValidators:true}
+        );
+        res.status(200).json({
+            success:true,
+            message:"User Updated Successfully",
+            data:{user}
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            error:error.message
+        });
+    }
+};
+
+export const deleteProfile=async(req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.user._id);
+        res.status(200).json({
+            success:true,
+            message:"User Deleted Successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            error:error.message
+        });
+    }
+};
+export const getAllUsers=async (req,res)=>{
+    try {
+        const users=await User.find().sort({createdAt: -1});
+        res.status(200).json({
+            success:true,
+            count:users.length,
+            data:{users}
+        });
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            error:error.message
+        });
+    }
+}
