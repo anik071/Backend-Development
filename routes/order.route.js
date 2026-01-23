6fcaf88f2cb1d7966437fb5cbba13b4719cb02fe
Abdul Kader Anik <153476736+anik071@users.noreply.github.com>
2026-01-24 01:58:59 +0600

    Update order.route.js

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import {
-    getOrders,
-    getOrder,
-    createOrder,updateOrder,deleteOrder
+    checkOut,getmyOrders,getOrderById,getAllOrders,updateOrderStatus,cancleOrder
 }from "../controllers/order.controller.js";
-
+import { authenticate,authorizeAdmin,validate } from '../middleware/user.middleware.js';
+import { orderValidator} from "../validators/order.validator.js"
 const router=express.Router();
-
-router.get("/orders",getOrders);
-router.get("/orders/:id",getOrder);
-router.post("/orders",createOrder);
-router.put("/orders/:id",updateOrder);
-router.delete("/orders/:id",deleteOrder);
+router.get("/orders",authenticate,authorizeAdmin,getAllOrders);
+router.get("/orders/me",authenticate,getmyOrders);
+router.get("/orders/:id",authenticate,getOrderById);
+router.post("/orders",authenticate,validate(orderValidator),checkOut);
+router.put("/orders/:id",updateOrderStatus);
+router.delete("/orders/:id",cancleOrder);
 export default router;
