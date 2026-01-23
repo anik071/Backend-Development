bd3cab2825965941496159b59eb1e8f4c2b9639e
Abdul Kader Anik <153476736+anik071@users.noreply.github.com>
2026-01-24 01:59:13 +0600

    Refactor cart routes to include authentication and new actions

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -2,14 +2,15 @@ import express from 'express';
 import {
     getCarts,
     getCart,
-    createCart,updateCart,deleteCart
+    addToCart,clearCart,removeFromCart
 }from "../controllers/cart.controller.js";
-
+import { authenticate,authorizeAdmin,validate } from '../middleware/user.middleware.js';
+import { addToCartValidator ,removeFromCartValidator} from "../validators/cart.validator.js"
 const router=express.Router();
 
-router.get("/carts",getCarts);
-router.get("/carts/:id",getCart);
-router.post("/carts",createCart);
-router.put("/carts/:id",updateCart);
-router.delete("/carts/:id",deleteCart);
+router.get("/carts",authenticate,authorizeAdmin,getCarts);
+router.get("/cart",authenticate,getCart);
+router.post("/carts/add",authenticate,validate(addToCartValidator),addToCart);
+router.post("/carts/rem",authenticate,validate(removeFromCartValidator),removeFromCart);
+router.post("/carts/clear",authenticate,clearCart);
 export default router;
