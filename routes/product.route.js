9a38e8dd2c9d387192739c4131a550656b2a8393
Shahu1234 <shahajadi135@gmail.com>
2026-01-24 01:58:40 +0600

    Update product.route.js

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,12 +2,14 @@ import express from "express";
 import{
     getProducts,getProduct,createProduct,updateProduct,deleteProduct
 } from "../controllers/product.controller.js";
+import { authenticate,authorizeAdmin, validate } from "../middleware/user.middleware.js";
+import {productValidator} from "../validators/product.validator.js"
 
 const router=express.Router();
-router.get("/products",getProducts);
-router.get("/products/:id",getProduct);
-router.post("/products",createProduct);
-router.put("/products/:id",updateProduct);
-router.delete("/products/:id",deleteProduct);
+router.get("/product",authenticate,getProduct);
+router.get("/products",authenticate,authorizeAdmin,getProducts);
+router.post("/products",authenticate,authorizeAdmin,validate(productValidator),createProduct);
+router.put("/products/:id",authenticate,authorizeAdmin,validate(productValidator),updateProduct);
+router.delete("/products/:id",authenticate,authorizeAdmin,deleteProduct);
 
 export default router;
